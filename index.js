16a7c66f97c39444fbc556f882edcabc9c25b5bb
Nadim Absi <absinad1@gmail.com>
2022-10-19 09:52:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+var customerName = "bob"
+
+function upperCaseCustomerName(upper="BOB"){
+     customerName=upper
+}
+function setBestCustomer(){
+     bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
+}
+const leastFavoriteCustomer ="Ellie"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Ellie again"
+}
 // Write your solution in this file!
